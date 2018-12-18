da15e62e06ca8b72a2822474a9c59954b5fe35a2
Navayuvan-NS <ns.navayuvan@gmail.com>
2018-12-19 01:47:47 +0530

    19.12.2018

---
+++ Pokedex.py
@@ -0,0 +1,21 @@
+# Importing required libraries
+
+import urllib2
+import urllib
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+import requests
+import lxml
+"""
+f = open("all_pokemon.txt","r")
+
+for x in f:
+	if (x != ""):
+		response = urllib2.urlopen("https://www.pokemon.com/us/pokedex/"+x)
+		page_source = response.read()
+"""
+
+url = "https://www.pokemon.com/us/pokedex/bulbasaur"
+
+response = urllib
+
